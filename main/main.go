00d7b14147643e4c42cba861a1713e80bc41a1f3
awaw <awaw@nandalu.idv.tw>
2013-09-15 22:14:46 +0800

    bind to OPENSHIFT_DIY_IP

--- main/main.go
+++ main/main.go
@@ -8,18 +8,26 @@ import (
 )
 
 const (
+	defaultHost = "127.0.0.1"
+	hostUsage   = "host to bind to"
 	defaultPort = 3000
 	portUsage   = "port to bind to"
 )
 
+var host string
 var port int
 
 func init() {
+	flag.StringVar(&host, "host", defaultHost, hostUsage)
+	flag.StringVar(&host, "h", defaultHost, hostUsage)
 	flag.IntVar(&port, "port", defaultPort, portUsage)
 	flag.IntVar(&port, "p", defaultPort, portUsage)
 }
 
 func main() {
 	flag.Parse()
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf("%v:%d", host, port), nil)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
 }
