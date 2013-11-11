036318acc6ed691143593dbdc0b120f45a969bb2
awaw <awaw@nandalu.idv.tw>
2013-11-12 03:53:04 +0800

    signaling is working, but video freezes

--- experimental.go
+++ experimental.go
@@ -1,9 +1,8 @@
 package geochat
 
 import (
-	"fmt"
+	"encoding/json"
 	"html/template"
-	"io/ioutil"
 	"net/http"
 	"sync"
 )
@@ -13,37 +12,43 @@ var g_webrtcMap = struct {
 	m map[string]chan recvMsg_t
 }{m: make(map[string]chan recvMsg_t)}
 
-type webrtcDataJs_t struct {
-	RoomSize int
-}
-type webrtcData_t struct {
-	Js webrtcDataJs_t
-}
-
 func webrtc(w http.ResponseWriter, r *http.Request) {
 	g_webrtcMap.RLock()
-	l := len(g_webrtcMap.m)
+	members := make([]string, 0)
+	for token, _ := range g_webrtcMap.m {
+		members = append(members, token)
+	}
 	g_webrtcMap.RUnlock()
 
 	t, _ := template.ParseFiles("tmpl/webrtc.html")
-	data := webrtcData_t{
-		Js: webrtcDataJs_t{
-			RoomSize: l,
-		},
+	data := struct {
+		Members []string
+	}{
+		Members: members,
 	}
 	t.Execute(w, data)
 }
 
 func webrtcTransmitter(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
+	decoder := json.NewDecoder(r.Body)
+	data := struct {
+		Destinations []string
+		Body         string
+	}{}
+	err := decoder.Decode(&data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
 	g_webrtcMap.RLock()
-	for _, channel := range g_webrtcMap.m {
+	for _, destination := range data.Destinations {
+		channel, ok := g_webrtcMap.m[destination]
+		if !ok {
+			continue
+		}
 		select {
-		case channel <- recvMsg_t{"", body}:
+		case channel <- recvMsg_t{"", []byte(data.Body)}:
 		default:
 		}
 	}
@@ -54,13 +59,13 @@ const leaveMsg = "leave"
 
 func webrtcLeaveSource(w http.ResponseWriter, r *http.Request) {
 	parser := paramParser{R: r, W: w}
-	username := parser.RequiredStringParam("username")
+	token := parser.RequiredStringParam("token")
 	if parser.Err != nil {
 		return
 	}
 
 	g_webrtcMap.RLock()
-	channel, ok := g_webrtcMap.m[username]
+	channel, ok := g_webrtcMap.m[token]
 	g_webrtcMap.RUnlock()
 	if ok {
 		select {
@@ -70,19 +75,19 @@ func webrtcLeaveSource(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func webrtcSource(w http.ResponseWriter, r *http.Request) {
-	username := string(randByteSlice())
+	token := string(randByteSlice())
 	channel := make(chan recvMsg_t, 32)
 	g_webrtcMap.Lock()
-	g_webrtcMap.m[username] = channel
+	g_webrtcMap.m[token] = channel
 	g_webrtcMap.Unlock()
 	defer func() {
 		g_webrtcMap.Lock()
-		delete(g_webrtcMap.m, username)
+		delete(g_webrtcMap.m, token)
 		g_webrtcMap.Unlock()
 	}()
 
 	sse := NewServerSideEventWriter(w)
-	sse.Write([]byte(`{"type":"username", "username":"` + username + `"}`))
+	sse.Write([]byte(`{"type":"token", "token":"` + token + `"}`))
 L:
 	for {
 		select {
